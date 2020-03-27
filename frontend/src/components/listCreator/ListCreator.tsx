84b4bb1d8298aafc40e21f678b0efec8f21f6998
Krzysztof Kąkol <krix38@gmail.com>
2020-03-27 19:46:36 +0100

    added list creation error handling

--- frontend/src/components/listCreator/ListCreator.tsx
+++ frontend/src/components/listCreator/ListCreator.tsx
@@ -1,20 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import {ListModel} from "../../services/api/model/ListModel";
 import {Redirect} from 'react-router-dom';
-import {ListService} from "../../services/api/service/ListService";
+import {ListService, ListError, isListError} from "../../services/api/service/ListService";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import {useLoaderStyles} from './useLoaderStyles';
+import {ListErrorModal} from '../listErrorModal/ListErrorModal';
 
 export const ListCreator = () => {
 
     const [createdList, setCreatedList] = useState<ListModel>();
+    const [displayError, setDisplayError] = useState<ListError>();
     const classes = useLoaderStyles();
 
     useEffect(() => {
         if(!createdList){
-            ListService.createList().then(list => setCreatedList(list))
+            ListService.createList()
+            .then(list => setCreatedList(list))
+            .catch(error => {
+                if(isListError(error)){
+                    setDisplayError(error);
+                }
+            });
         }
     });
+
+    if (displayError) {
+        return (<ListErrorModal displayError={displayError}/>);
+    }
 
     return (
         <>
